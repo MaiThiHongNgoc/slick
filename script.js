3058249545841340d7d1d858cc6f3ad7ac889ad5
MaiThiHongNgoc <146064815+MaiThiHongNgoc@users.noreply.github.com>
2023-11-09 02:00:23 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded",function(){
+    $('.sanphambanchay').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='bx bx-chevron-left' ></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='bx bx-chevron-right' ></i></button>",
+      });
+})
